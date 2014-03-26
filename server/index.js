54a418b7a9c1facbba962672222cdf23f247e36b
Johannes Brodwall <johannes@brodwall.com>
2014-03-26 08:14:04 +0100

    Create service for workers

--- server/index.js
+++ server/index.js
@@ -3,7 +3,14 @@ var app = express();
 
 app.use(express.static(__dirname + '/../public'));
 
+var workers = [
+  { displayName: 'Lasantha', designation: 'BA' },
+  { displayName: 'Sankalpa', designation: 'SSE' }
+];
 
+app.get('/api/workers', function(req, res) {
+  res.send(workers);
+});
 
 
 var port = process.env.PORT || 3000;
